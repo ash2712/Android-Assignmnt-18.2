57e9631b72f06d23c68bdc06394b7e078b63a985
ash2712 <spellguru@gmail.com>
2017-11-21 20:54:21 +0530

    Update MySMSReceiver.java

--- src/main/java/com/example/ayush/smsrecieveapp/MySMSReceiver.java
+++ src/main/java/com/example/ayush/smsrecieveapp/MySMSReceiver.java
@@ -13,18 +13,14 @@ public class MySMSReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         // if the action received is equal to message received
-        Log.e("intetnt",intent.getAction());
-        if(intent.getAction().equals("SMS_RECEIVED_ACTION")){
-            //creating bundle
+        if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
             final Bundle bundle = intent.getExtras();
 
-            try {
-                if (bundle != null) {
-                    //creating object
+            try{
+                if (bundle!=null){
                     final Object[] pdusObj = (Object[]) bundle.get("pdus");
                     String phoneNumber = null;
-                    for (int i = 0; i < pdusObj.length; i++) {
-
+                    for(int i  = 0;i < pdusObj.length;i++){
                         SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
                         //getting sender's phone number
                         phoneNumber = currentMessage.getOriginatingAddress();
@@ -33,15 +29,15 @@ public class MySMSReceiver extends BroadcastReceiver {
                         //getting Sender's text message
                         String message = currentMessage.getDisplayMessageBody();
 
-                        Log.e("SmsReceiver", "senderNum: "+ senderNum + "; message: " + message);
+                        Log.i("SmsReceiver", "senderNum: "+ senderNum + "; message: " + message);
                         //displaying toast
                         Toast.makeText(context, "senderNum: "+ senderNum + ", message: " + message, Toast.LENGTH_LONG).show();
 
-                    } // end for loop
-                } // bundle is null
-
-            } catch (Exception e) {
-                Log.e("ReceiveSMS", "Exception smsReceiver" + e);
+                    }
+                }
+            }
+            catch (Exception e){
+                e.printStackTrace();
             }
         }
     }
